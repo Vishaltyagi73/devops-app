3e18618eeb631824da41d7e4c9632e845538d411
Tanusha-tyagi <tanusha.2428cs583@kiet.edu>
2026-04-17 15:58:38 +0530

    initial node devops app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", message: "Server is running" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
